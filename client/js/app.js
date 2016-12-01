b7990f0cfc1c34c948b16dede9eb476d02e7d660
bonnybon <roberto.bonera@unimi.it>
2016-12-01 16:35:24 +0100

    finito, forse

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,62 @@
 var app = angular.module("appRotture", ['lbServices']);
 
-app.controller("controllerRotture", ["$scope", 
-	function($scope)
+app.controller("controllerRotture", ["$scope", "Todo",
+	function($scope, Todo)
 	{
+		$scope.todos = [];
+		$scope.todosdone = [];
+		$scope.newTodo=
+		{
+			name:"",
+			desc:"",
+			important:false
+		};
 
+		$scope.addTodo = function()
+		{
+			console.log($scope.newTodo);
+		};
+
+		$scope.delTodo = function(todoId)
+		{
+			console.log("cancella"+todoId)
+		};
+
+		$scope.execTodo = function(todoId)
+		{
+			for(i = 0; i < $scope.todos.length; i++)
+			{
+				if($scope.todos[i].id==todoId)
+				{
+					$scope.todos[i].done=true;
+					Todo.upsert($scope.todos[i]);
+					$scope.todos=[];
+					$scope.todosdone=[];
+					getTodos();
+				}
+			}
+			console.log("esegue"+todoId)
+		};
+
+		function getTodos()
+		{
+			Todo.find()
+			.$promise
+			.then(function(results)
+			{
+				for(i = 0; i < results.length; i++)
+				{
+					if(results[i].done == false)
+					{
+						$scope.todos.push(results[i]);	
+					} else
+					{
+						$scope.todosdone.push(results[i]);	
+					}
+				}
+				console.log(results)
+			})
+		}
+
+		getTodos();
 	}]);
